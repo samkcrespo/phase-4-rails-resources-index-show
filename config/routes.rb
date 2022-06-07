c37f394ee8d770233f667b8d6db172fd491eadcc
Samantha Crespo <samkcrespo@gmail.com>
2022-06-06 19:13:06 -0700

    commit tree 88a1d7403ef2ce6b85c015aa036fc3028f860d09

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+
+  # get '/birds', to: 'birds#index'
+  # get '/birds/:id', to: 'birds#show'
+  resources :birds, only: [:index, :show]
+## With just one line of code — resources :birds — Rails created all the RESTful routes we need and mapped them to the appropriate controller action! 
+
 end
